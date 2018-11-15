7592ca39e82ea700f66e71995c8ba33240996807
Thomas Defise <t.defise@gmail.com>
2018-11-15 08:30:29 +0100

    working

--- pyTraktorPlaylistExporter.py
+++ pyTraktorPlaylistExporter.py
@@ -6,32 +6,42 @@ COLLECTION = r"C:\Users\Thomas.defise\Documents\Native Instruments\Traktor 2.11.
 TRACK_LIST = []
 
 
+def parse_collection(playlist):
+    parser = ET.XMLParser(encoding="utf-8")
+    tree = ET.parse(COLLECTION, parser=parser)
+
+    for a in tree.iter(tag="NODE"):
+        if a.get("NAME") == playlist:
+            for z in a.iter(tag="PRIMARYKEY"):
+                TRACK_LIST.append(z.get("KEY").replace("/:", "\\"))
+
+    return TRACK_LIST
+
+
+def arg_processing(argv):
+    playlist = ""
+    try:
+        opts, args = getopt.getopt(argv, "hp:", ["playlist="])
+    except getopt.GetoptError:
+        print("pyTraktorPlaylistExporter__.py -p <TraktorPlaylist>")
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            'test.py -i <inputfile> -o <outputfile>'
+            sys.exit()
+        elif opt in ("-p", "--playlist"):
+            return arg
+
+
 def main(argv):
     if len(argv) == 0:
         print("pyTraktorPlaylistExporter.py -p <TraktorPlaylist>")
-    else:
-        playlist = ""
-        try:
-            opts, args = getopt.getopt(argv, "hp:", ["playlist="])
-        except getopt.GetoptError:
-            print("pyTraktorPlaylistExporter__.py -p <TraktorPlaylist>")
-            sys.exit(2)
-        for opt, arg in opts:
-            if opt == '-h':
-                'test.py -i <inputfile> -o <outputfile>'
-                sys.exit()
-            elif opt in ("-p", "--playlist"):
-                playlist = arg
+        sys.exit(2)
 
-        parser = ET.XMLParser(encoding="utf-8")
-        tree = ET.parse(COLLECTION, parser=parser)
+    playlist = arg_processing(argv)
+    TRACK_LIST = parse_collection(playlist)
 
-        for a in tree.iter(tag="NODE"):
-            if a.get("NAME") == playlist:
-                for z in a.iter(tag="PRIMARYKEY"):
-                    TRACK_LIST.append(z.get("KEY").replace("/:", "\\"))
-
-        print(TRACK_LIST)
+    print(TRACK_LIST)
 
 
 if __name__ == '__main__':
